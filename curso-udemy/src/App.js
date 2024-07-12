0fe65b7afd81c9566c39765a659d43904b1830ff
MIGUEL SANCHEZ <miguelsc10@hotmail.com>
2024-07-11 20:48:21 -0400

    practicas de useState

--- curso-udemy/src/App.js
+++ curso-udemy/src/App.js
@@ -1,16 +1,30 @@
 import React, { useState, useEffect } from "react";
+import Boton from "./components/Boton/index";
+import Result from "./components/Result/Congratulations";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [result, setResult] = useState(false);
 
   useEffect(() => {
     document.title = `You clicked ${count} times`;
   });
-
+  const [valor, setValor] = useState(0);
+  const handleClick = () => {
+    setValor(valor + 1);
+    if (valor > 5) {
+      setResult(true);
+    }
+  };
   return (
     <div>
-      <p>You clicked {count} times.</p>
-      <button onClick={() => setCount(count + 1)}>Click Me</button>
+      {/* <p>You clicked {count} times.</p>
+      <button onClick={() => setCount(count + 1)}>Click Me</button> */}
+      <h1>Varias formas de usar useStte</h1>
+      <h2>{valor}</h2>
+      <Boton setValor={setValor} valor={valor} />
+      <button onClick={handleClick}>HOla +1</button>
+      {result && <Result />}
     </div>
   );
 }
--- curso-udemy/src/components/Boton/index.js
+++ curso-udemy/src/components/Boton/index.js
@@ -1,5 +1,16 @@
 import React from "react";
 
-export default function Boton(props) {
-  return <button type="button" class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Mi nuevo boton {props.numero}</button>;
-}
+const Boton = ({ setValor, valor }) => {
+  return (
+    <button
+      onClick={() => {
+        setValor(valor + 2);
+      }}
+      type="button"
+      class="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+    >
+      Mi boton + 2
+    </button>
+  );
+};
+export default Boton;
